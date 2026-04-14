29bf9462fb6b0fb4923e7d3e9b04c53a72784db4
SamHwu <samhwu0411@gmail.com>
2026-04-14 23:00:36 +0800

    調整數值Ｄetail 頁面 ＲＷＤ

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2095351837" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1242214985" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
